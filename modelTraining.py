340fe4ced2a50ce7e437f41aa28f31aba30cd895
unknown <ales.kert@gmail.com>
2021-08-24 14:10:28 +0200

    avgž

--- modelTraining.py
+++ modelTraining.py
@@ -339,7 +339,7 @@ else :
     model_dir = 'F:/Diploma/models/'
 
 if cluster_mode :
-    os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 
 build_model = True
@@ -355,7 +355,7 @@ num_classes = 2
 batch_size = 12
 num_epochs = 40
 
-input_type = InputType.STOKES
+input_type = InputType.STOKES_CALC_PLUS
 
 images = sorted(
     [
