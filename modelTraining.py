f56fe79bac24d2948d43a47d81ea9751ef0c0ab2
unknown <ales.kert@gmail.com>
2021-08-24 16:19:17 +0200

    4c

--- modelTraining.py
+++ modelTraining.py
@@ -343,7 +343,7 @@ else :
     model_dir = 'F:/Diploma/models/'
 
 if cluster_mode :
-    os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 
 build_model = True
@@ -359,7 +359,7 @@ num_classes = 2
 batch_size = 12
 num_epochs = 60
 
-input_type = InputType.FOUR_CHANNEL
+input_type = InputType.STOKES
 
 images = sorted(
     [
