654deb1bfa0cfa8d1735eba4459ef97f0db6a5c8
unknown <ales.kert@gmail.com>
2021-08-25 13:25:59 +0200

    deinterlace

--- polarizedImgRead.py
+++ polarizedImgRead.py
@@ -16,8 +16,8 @@ dataSet = os.listdir(path)
 
 
 for file in dataSet:
-    fileName = file
-    image = np.array(load_img(file, color_mode="grayscale"))
+    fileName = path + file
+    image = np.array(load_img(fileName, color_mode="grayscale"))
     
     h = len(image)
     w = len(image[0])
