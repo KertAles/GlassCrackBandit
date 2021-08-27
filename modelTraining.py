d022bab2961c5e78a30acb6025293004988172cf
unknown <ales.kert@gmail.com>
2021-08-27 12:47:38 +0200

    stokes 1x1

--- modelTraining.py
+++ modelTraining.py
@@ -372,7 +372,7 @@ else :
     model_dir = 'F:/Diploma/models/'
 
 if cluster_mode :
-    os.environ["CUDA_VISIBLE_DEVICES"]="2"
+    os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 
 build_model = True
@@ -388,7 +388,7 @@ num_classes = 2
 batch_size = 12
 num_epochs = 60
 
-input_type = InputType.STOKES
+input_type = InputType.STOKES_CALC_PLUS
 
 """
 images = sorted(
